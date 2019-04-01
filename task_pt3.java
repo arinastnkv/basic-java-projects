cc73277da2f4eb40d01a410f0a3a99b1237c2cbc
Arina Sitnikova <arinasitnikova@gmail.com>
2019-04-01 18:13:37 +0300

    Getting url of observations with less than 50 ms response time + implementing progress bar

--- task_pt3.java
+++ task_pt3.java
@@ -1,23 +1,19 @@
 package as.project;
 
-import javax.swing.plaf.synth.SynthScrollBarUI;
-import java.io.*;
 
+import java.io.*;
 import java.io.BufferedReader;
-import java.io.FileNotFoundException;
-import java.io.FileReader;
-import java.io.IOException;
-import java.util.HashMap;
-import java.util.Map;
-import java.util.TreeMap;
+import me.tongfei.progressbar.*;
 
 public class task_pt3 {
 
     public static void main(String[] args) {
-	// write your code here
-        try{
-            FileInputStream logfile = new FileInputStream("nginx.log");
-            BufferedReader br = new BufferedReader(new InputStreamReader(logfile));
+
+        ProgressBarBuilder pbb = new ProgressBarBuilder()
+                .setTaskName("Reading")
+                .setUnit(" MB", 1048576);
+
+        try (BufferedReader br = new BufferedReader(new InputStreamReader(ProgressBar.wrap(new FileInputStream("nginx.log"), pbb)))) {
             String line;
             String cvsSplitBy = ",";
 
@@ -30,33 +26,24 @@ public class task_pt3 {
 
             boolean firstLine = true;
 
-            //List<String> lines = new ArrayList<String>();
 
             while ((line = br.readLine()) != null) {
                 if (firstLine) {
                     firstLine = false;
                     continue;
                 }
-                //Matcher m1 = p.matcher(line);
-                //while (m1.find()) {
-                  //  pw.println(m1.group());
+
                 String[] url = line.split(cvsSplitBy);
                 if (!url[url.length - 4].isEmpty()) {
                     if (((Float.valueOf(url[url.length - 4])) < 0.050)) {
-                        //lines.add(url[url.length - 6] + url[url.length - 5]);
                         pw.println(url[url.length - 6] + url[url.length - 5]);
                     }
                 }
-
                 }
-            //}
 
             pw.close();
-            logfile.close();
         } catch(Exception e) {
             System.out.println("Error: " + e);
         }
-
-
     }
 }
